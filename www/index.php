fc1bdcc12afb02ff7ff8fe1cc79117727f05e44f
Mendez <emendez@emendez-mac.local>
2018-07-05 09:43:27 -0500

    Index change 2

--- www/index.php
+++ www/index.php
@@ -16,7 +16,8 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
+    <img id="logo" src="logo.png" />
+    <h1>Demo at Intel</h1>
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
